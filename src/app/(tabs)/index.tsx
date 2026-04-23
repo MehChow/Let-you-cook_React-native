6cafc2b028f9543cd60ae6219ad744f8cac62665
MichaelChow-0604 <michael.chow@stoneleighsolution.com>
2026-04-23 12:47:42 +0800

    fix: Setup React Native Reusables with Uniwind

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,11 +1,9 @@
-import { Home as HomeIcon } from "@/components/Icon";
 import { Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-red-200">
-      <Text>Home</Text>
-      <HomeIcon size={28} color="black" />
+      <Text className="text-2xl font-bold">Hello World</Text>
     </View>
   );
 }
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,21 @@
+import { NAV_THEME } from "@/lib/theme";
+import { ThemeProvider } from "@react-navigation/native";
+import { PortalHost } from "@rn-primitives/portal";
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { useUniwind } from "uniwind";
 import "../global.css";
 
 export default function RootLayout() {
+  const { theme } = useUniwind();
+
   return (
-    <Stack initialRouteName="(tabs)">
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <ThemeProvider value={NAV_THEME[theme ?? "light"]}>
+      <StatusBar style={theme === "dark" ? "light" : "dark"} />
+      <Stack initialRouteName="(tabs)">
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+      <PortalHost />
+    </ThemeProvider>
   );
 }
