80b39086583900770d65b5e5a8faf9f3d085d06b
MichaelChow-0604 <michael.chow@stoneleighsolution.com>
2026-04-23 12:53:46 +0800

    remove: duplicated uniwind-types.d.ts file

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-red-200">
-      <Text className="text-2xl font-bold">Hello World</Text>
+      <Text>Hello World</Text>
     </View>
   );
 }
