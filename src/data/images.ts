a7e8eed2606671d941789f1e74fab33a35c16f62
MehChow <mehchow0604@gmail.com>
2026-07-10 16:37:36 +0800

    assets: converted png to webp

--- src/data/images.ts
+++ src/data/images.ts
@@ -19,6 +19,6 @@ export const images = {
   authForgotPassword2: require("@/assets/images/auth/forget-password2.webp"),
   authEmailOtp: require("@/assets/images/auth/email-otp.webp"),
   authCreateNewPassword: require("@/assets/images/auth/create-new-password.webp"),
-  authCreateAccount: require("@/assets/images/auth/notebook_transparent.png"),
+  authCreateAccount: require("@/assets/images/auth/notebook_transparent.webp"),
   authGoogleLogin: require("@/assets/images/auth/google-icon.svg"),
 } as const;
